6af323a7501804021b9346c04e699fa8d14247de
Caio <caioflavio2@hotmail.com.br>
2021-11-28 22:42:42 -0300

    :sparkles: feat(homepage): adding homepage view components

---
+++ smts/resources/views/web/homepage/components/features.blade.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <div class="section">
+
+        <!--   Icon Section   -->
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center orange-text"><i class="material-icons">flash_on</i></h2>
+                    <h5 class="center">Speeds up development</h5>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center orange-text"><i class="material-icons">group</i></h2>
+                    <h5 class="center">User Experience Focused</h5>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center orange-text"><i class="material-icons">settings</i></h2>
+                    <h5 class="center">Easy to work with</h5>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <br><br>
+</div>
