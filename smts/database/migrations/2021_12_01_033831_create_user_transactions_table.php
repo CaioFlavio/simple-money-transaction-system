7ff27b3db92af65e52a2bb3df336b4580e40e476
Caio <caioflavio2@hotmail.com.br>
2021-12-01 01:00:38 -0300

    :sparkles: :card_file_box: feat(db): adding migration for data persistance to existing contexts

---
+++ smts/database/migrations/2021_12_01_033831_create_user_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sender_user_id');
+            $table->unsignedBigInteger('receiver_user_id');
+            $table->tinyInteger('is_authorized');
+            $table->float('value');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_transactions');
+    }
+}
