f3ddac4c04b22a47b10fc3753eec78ef8b48e32d
Caio <caioflavio2@hotmail.com.br>
2021-11-30 23:53:25 -0300

    :sparkles: feat(user): adding user wallet context interfaces

---
+++ smts/app/Domain/Users/UserFunds/UserWallet/Contracts/Repositories/UserWalletRepositoryInterface.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Domain\Users\UserFunds\UserWallet\Contracts\Repositories;
+
+interface UserWalletRepositoryInterface
+{
+    /**
+     * @param $id
+     * @return array
+     */
+    public function getData($id) : array;
+
+    /**
+     * @param $id
+     * @return array
+     */
+    public function canSendFunds($id) : array;
+
+    /**
+     * @param $id
+     * @return array
+     */
+    public function canReceiveFunds($id) : array;
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function hasFunds($id) : bool;
+
+    /**
+     * @param $user_id
+     * @param float $value
+     * @return bool
+     */
+    public function addFunds($user_id, float $value) : bool;
+
+    /**
+     * @param $user_id
+     * @param float $value
+     * @return bool
+     */
+    public function withdrawFunds($user_id, float $value) : bool;
+
+    /**
+     * @param $id
+     * @return float
+     */
+    public function getBalance($id) : float;
+}
