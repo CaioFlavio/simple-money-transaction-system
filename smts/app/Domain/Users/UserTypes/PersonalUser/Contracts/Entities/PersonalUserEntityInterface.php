84faecee0362bad7822c1634c82af78274eb7730
Caio <caioflavio2@hotmail.com.br>
2021-11-30 23:51:32 -0300

    :sparkles: feat(users): adding persoal user context interfaces

---
+++ smts/app/Domain/Users/UserTypes/PersonalUser/Contracts/Entities/PersonalUserEntityInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Domain\Users\UserTypes\PersonalUser\Contracts\Entities;
+
+use App\Domain\Users\BaseUser\Contracts\Entities\UserEntityInterface;
+
+interface PersonalUserEntityInterface extends UserEntityInterface
+{
+    /**
+     * @return string
+     */
+    public function getTypeCode() : string;
+}
