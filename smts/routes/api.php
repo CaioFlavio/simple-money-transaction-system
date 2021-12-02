191d03a55adb84eaae1a5aea8d36e71102de3f08
Caio <caioflavio2@hotmail.com.br>
2021-12-02 07:26:52 -0300

    :wrench: feat(wallet): registering routes for user wallet api

--- smts/routes/api.php
+++ smts/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\User\UserRestController;
+use App\Http\Controllers\Api\User\UserWalletRestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('users', [UserRestController::class, 'create']);
 Route::post('users/auth', [UserRestController::class, 'auth']);
+
+Route::middleware(['jwt.auth'])->group(function(){
+    Route::post('users/{id}/add_funds', [UserWalletRestController::class, 'add']);
+    Route::post('users/{id}/withdraw_funds', [UserWalletRestController::class, 'withdraw']);
+});
