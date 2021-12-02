02ed486698ee44cb926e7bca35cc65f4393f64dd
Caio <caioflavio2@hotmail.com.br>
2021-12-02 00:58:08 -0300

    :sparkles: feat(api): adding infraestructure resources for api

---
+++ smts/app/Infrastructure/Services/User/UserApi.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Infrastructure\Services\User;
+
+use App\Infrastructure\User\Contracts\UserApiInterface;
+
+class UserApi implements UserApiInterface
+{
+    public function __construct(
+
+    ) {
+
+    }
+}
