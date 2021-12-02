12090faa9aeab9221d7f876b6a2b05bd6498a3ee
Caio <caioflavio2@hotmail.com.br>
2021-12-02 00:57:10 -0300

    :wrench: feat(app): defining interface mapping for current classes

--- smts/app/Providers/AppServiceProvider.php
+++ smts/app/Providers/AppServiceProvider.php
@@ -2,6 +2,16 @@
 
 namespace App\Providers;
 
+use App\Domain\Users\UserTypes\BaseUser\Entities\UserEntity;
+use App\Domain\Users\UserTypes\BaseUser\Contracts\Entities\UserEntityInterface;
+use App\Domain\Users\UserTypes\BusinessUser\Contracts\Repositories\BusinessUserRepositoryInterface;
+use App\Domain\Users\UserTypes\BusinessUser\Repositories\BusinessUserRepository;
+use App\Domain\Users\UserTypes\BusinessUser\Contracts\Repositories\BusinessAccountRepositoryInterface;
+use App\Domain\Users\UserTypes\BusinessUser\Repositories\BusinessAccountRepository;
+use App\Domain\Users\UserTypes\PersonalUser\Contracts\Repositories\PersonalUserRepositoryInterface;
+use App\Domain\Users\UserTypes\PersonalUser\Repositories\PersonalAccountRepository;
+use App\Domain\Users\UserTypes\PersonalUser\Contracts\Repositories\PersonalAccountRepositoryInterface;
+use App\Domain\Users\UserTypes\PersonalUser\Repositories\PersonalUserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(UserEntityInterface::class, UserEntity::class);
+        $this->app->bind(PersonalAccountRepositoryInterface::class, PersonalAccountRepository::class);
+        $this->app->bind(BusinessAccountRepositoryInterface::class, BusinessAccountRepository::class);
+        $this->app->bind(PersonalUserRepositoryInterface::class, PersonalUserRepository::class);
+        $this->app->bind(BusinessUserRepositoryInterface::class, BusinessUserRepository::class);
     }
 
     /**
